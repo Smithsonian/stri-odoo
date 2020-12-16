c195bcd38cd9252ab30eedc6d74f1e77ca142eb4
Luis Domínguez <luis.dominguez19@yahoo.es>
2020-12-16 11:03:21 -0500

    added auth_oauth

--- hs_custom_develop/controller/__init__.py
+++ hs_custom_develop/controller/__init__.py
@@ -2,3 +2,4 @@
 
 
 from . import account_payment
+from . import auth_oauth
---
+++ hs_custom_develop/controller/auth_oauth.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import http, _
+from odoo.exceptions import AccessError, MissingError
+from odoo.addons.auth_oauth.controller.main import OAuthController
+from odoo.http import request
+
+import logging
+_logger = logging.getLogger(__name__)
+
+class VSOController(OAuthController):
+	@http.route('/auth_oauth/signin', type='http', auth='none')
+	@fragment_to_query_string
+	def signin(self, **kw):
+		_logger.info("Los parametos devueltos por VSO son")
+		_logger.info(kw)
+		return super(VSOController, self).signin(**kw)
