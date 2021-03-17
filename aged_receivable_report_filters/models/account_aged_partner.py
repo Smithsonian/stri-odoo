89a059b36e809f6012e1a41757def1d215017dfe
Luis Domínguez <luis.dominguez19@yahoo.es>
2021-03-17 14:22:38 -0500

    Update account_aged_partner.py

--- aged_receivable_report_filters/models/account_aged_partner.py
+++ aged_receivable_report_filters/models/account_aged_partner.py
@@ -77,9 +77,8 @@ class AccountAgedPartner(models.AbstractModel):
         """ Fetched selected analytic accounts and group from options and
             created final list of analytic accounts """
         if self.env.user.id in self.env.ref('analytic.group_analytic_accounting').users.ids:
-            if options.get('analytic_accounts') and options.get('analytic_group'):
-                analytic_groups_list = [int(group_id) for group_id in options.get('analytic_group')]
-                analtic_account_list = [int(account_id) for account_id in options.get('analytic_accounts')]
+            analytic_groups_list = [int(group_id) for group_id in options.get('analytic_group')]
+            analtic_account_list = [int(account_id) for account_id in options.get('analytic_accounts')]
             if analytic_groups_list:
                 analytic_account_ids = self.env['account.analytic.account'].search([('group_id.id', 'in', analytic_groups_list)]).ids
                 analtic_account_list = list(set(analtic_account_list) | set(analytic_account_ids))
